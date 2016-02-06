889fa0233208b33e41fe7dd99285c92f36c172b1
Aayush Bhutani <aayushb26@Macs-MacBook-Pro.local>
2016-02-06 00:34:06 -0500

    frontendwork

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pagename='Home')
+    return render_template('index.html', pagename="Home")
+
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
+@app.route('/Newuser')
+def newuser():
+    return render_template('newuser.html')
+
+@app.route('/Existinguser')
+def existinguser():
+    return render_template('existinguser.html')
+
+@app.route('/Play')
+def play():
+    return render_template('play.html')
+
+
+
+
+
 
 
 if __name__ == '__main__':
