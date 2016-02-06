e1e31a8ed17495fce1e7dbb92a7fa6a941818a95
kkleiven <kkleiven@andrew.cmu.edu>
2016-02-05 23:32:57 -0500

    Backend Registration/Login
    Added backend registration and login logic

--- app/models.py
+++ app/models.py
@@ -1,13 +1,31 @@
 from app import db
-
+from sqlalchemy.ext.hybrid import hybrid_property
+import hashlib
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    _password = db.Column(db.String(128), nullable=False)
     experience = db.Column(db.Integer, default=0)
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
 
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def _set_password(self, plaintext):
+        m = hashlib.sha512()
+        m.update(plaintext.encode())
+        self._password = m.digest()
+
+    def __init__(self, username, password):
+        self.name = username
+        self.password = password
+        self.experience = 0
+
+    def is_correct_password(self, plaintext):
+        return bcrypt.check_password_hash(self._password, plaintext)
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, abort, flash
+from flask import Blueprint, render_template, abort, flash, request
 
 from app.models import *
 from app.helpers import *
+from app.forms import *
 
 mod = Blueprint('snapples',
                 __name__,
@@ -16,6 +17,33 @@ def index():
     else:
         return render_template('index.html')
 
+@mod.route('login/', methods=['GET', 'POST'])
+def login():
+    form = UsernamePasswordForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #try to log them in
+        user = User.query.filter_by(username==form.username.data).first_or_404()
+        if user.is_correct_password(form.password.data):
+            #log in the user
+            userInfo = dict()
+            userInfo['username'] = user.username
+            userInfo['id'] = user.id
+            db_session['user-info'] = userInfo
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
+    
+@mod.route('register/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #register them
+        user = User(form.username.data, form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 @mod.route('upload/', methods=['GET', 'POST'])
 @loginRequired
